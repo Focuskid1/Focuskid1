00730e5e07f718cad6e17b043dd8a61538cf01a4
Focuskid1 <168267184+Focuskid1@users.noreply.github.com>
2024-07-29 08:10:23 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,25 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 120);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+};
+
+window.onscroll  = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
+
+function blogAlert() {
+    alert("Focus Room Tech Community - Coming Soon! We're building a vibrant community for tech enthusiasts, developers, and innovators. Stay tuned for our launch! Please check back soon.");
+}
+
+function contactAlert() {
+    alert("Your Message has been recevied, Focus tech customer service will send you a mail shortly!")
